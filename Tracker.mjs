cb7c01d726618829ab4324b110d7f87d8f971921
Vitor L Cavalcanti <vitorluizc@outlook.com>
2024-08-13 18:22:13 -0300

    Remove hj iframe element and prevent some errors with a stub object

--- Tracker.mjs
+++ Tracker.mjs
@@ -1,51 +1,59 @@
-// @ts-check
+import Hotjar from "https://unpkg.com/@hotjar/browser@1.0.9/dist/index.esm.js";
 
-// @ts-expect-error
-import Hotjar from 'https://unpkg.com/@hotjar/browser@1.0.9/dist/index.esm.js'
-
-const HOTJAR_VERSION = 6
+const HOTJAR_VERSION = 6;
 
 const HOTJAR_SITE_ID = 5093932;
 
 class Tracker {
-  #initialized = false
+  #initialized = false;
 
-  #trackers = /** @type {Map<string, number>} */ (new Map())
+  #trackers = new Map();
 
-  /** @param {string} feature */
   initialize(feature) {
-    const count = this.#trackers.get(feature) ?? 0
+    const count = this.#trackers.get(feature) ?? 0;
 
-    this.#trackers.set(feature, count + 1)
+    this.#trackers.set(feature, count + 1);
 
-    if (this.#initialized) return
+    if (this.#initialized) return;
+
+    // deletes previous stub hj object
+    if (window.hj && Symbol.for("is stub hj") in window.hj) delete window.hj;
 
     this.#initialized = Hotjar.init(HOTJAR_SITE_ID, HOTJAR_VERSION, {
-      debug: true
-    })
+      debug: true,
+    });
   }
 
-  /** @param {string} feature */
   finalize(feature) {
-    const count = (this.#trackers.get(feature) ?? 1) - 1
+    const count = (this.#trackers.get(feature) ?? 1) - 1;
 
     if (count === 0) {
-      this.#trackers.delete(feature)
+      this.#trackers.delete(feature);
     } else {
-      this.#trackers.set(feature, count)
+      this.#trackers.set(feature, count);
     }
 
-    if (this.#trackers.size > 0) return
+    if (this.#trackers.size > 0) return;
 
-    if (!this.#initialized) return
+    if (!this.#initialized) return;
 
-    const script = document.getElementById('hotjar-init-script')
-    script?.remove()
-    window.hj?.q?.splice(0, window.hj.q.length)
-    window.hj?.metrics?.stop()
-    delete window.hj?.metrics
-    delete window.hj
+    const script = document.getElementById("hotjar-init-script");
+    const iframe = document.querySelector("iframe[title=_hjSafeContext]");
+    script?.remove();
+    iframe?.remove();
+    window.hj?.q?.splice(0, window.hj.q.length);
+    window.hj?.metrics?.stop();
+    delete window.hj?.metrics;
+    delete window.hj;
+
+    // creates an stub hotjar object to avoid errors
+    window.hj = {
+      [Symbol.for("is stub hj")]: true,
+      tryCatch() {},
+    };
+
+    this.#initialized = false;
   }
 }
 
-export default Tracker
+export default Tracker;
